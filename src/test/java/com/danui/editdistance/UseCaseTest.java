6e88fac3d3e2cbbfe529e9937034bd9337d416b5
jin <hsienjin@gmail.com>
2016-05-02 12:22:31 +0800

    Add stub UseCaseTest.

---
+++ src/test/java/com/danui/editdistance/UseCaseTest.java
@@ -0,0 +1,12 @@
+package com.danui.editdistance;
+
+import org.junit.Test;
+
+public class UseCaseTest {
+
+    @Test
+    public void exampleUseCase() {
+        // TODO Write example use cases here.
+    }
+
+}
